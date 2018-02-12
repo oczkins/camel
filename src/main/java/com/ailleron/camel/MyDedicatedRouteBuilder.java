f8558a956ab141560bbe4ee4c995c6d003567270
woczkowski <woczkowski@itkontekst.pl>
2018-02-12 15:30:31 +0100

    step3

--- src/main/java/com/ailleron/camel/MyDedicatedRouteBuilder.java
+++ src/main/java/com/ailleron/camel/MyDedicatedRouteBuilder.java
@@ -14,20 +14,27 @@ import org.springframework.stereotype.Component;
  * @author Wojciech Oczkowski
  */
 @Component
-public class MyDedicatedRouteBuilder extends SpringRouteBuilder{
+public class MyDedicatedRouteBuilder extends SpringRouteBuilder {
 
     @Override
     public void configure() throws Exception {
-                        from("direct:callJsonTest")
-                        .to("log:fromJetty?showAll=true")
-                        .setBody(
-                                simple("{\"requestDate\":\"${date:now:yyyyMMdd}\"}"))
-                        .to("bean:myProcessor?method=myProcess")
-                        .to("http://tojestzlyurl/")
-                        .removeHeaders("*")
-                        .to("http://date.jsontest.com/")
-                        .log(LoggingLevel.ERROR, "logger.error", "panic! ${body}")
-                        ;
+        from("direct:callJsonTest")
+                .streamCaching()
+                .to("log:fromJetty?showAll=true")
+                .setBody(
+                        simple("{\"requestDate\":\"${date:now:yyyyMMdd}\"}"))
+                .to("bean:myProcessor?method=myProcess")
+                .multicast()
+                   .to("http://tojestzlyurl/")
+                   .to("direct:callDate")
+                .end()
+                .log(LoggingLevel.ERROR, "logger.error", "panic! ${body}");
+        
+                from("direct:callDate")
+                .removeHeaders("*")
+                .to("http://date.jsontest.com/");
+                        
+                
     }
-    
+
 }
